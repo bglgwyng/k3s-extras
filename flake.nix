507f59f308012aee7ccd24b014e9e03b7c46bb47
bgl gwyng <bgl@gwyng.com>
2026-02-21 23:35:57 +0900

    Add known hosts configuration for GitHub in OpenSSH service

--- flake.nix
+++ flake.nix
@@ -62,6 +62,11 @@
           systemd.services.k3s.restartTriggers = [
             config.services.k3s.containerdConfigTemplate
           ];
+
+          services.openssh.knownHosts = {
+            "github.com".publicKey =
+              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
+          };
         };
     };
 }
